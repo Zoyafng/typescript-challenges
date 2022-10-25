eefd2e2933d5f75ee05d96b138558b362ddb1c1d
周雅风 <zhouyafeng@authing.cn>
2022-10-25 19:48:53 +0800

    feat(ReadOnly): :fire: ReadOnly

---
+++ src/ReadOnly/readonly.ts
@@ -0,0 +1,12 @@
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+// interface Todo1 {
+//     title: string;
+//     description: string;
+//     completed: boolean;
+//     meta: {
+//       author: string;
+//     };
+//   }
